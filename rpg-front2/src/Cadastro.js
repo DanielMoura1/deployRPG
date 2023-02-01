a2ca77b310dccfdec597e6f4e2352479802222b6
daniel <daniel.bnh13@gmail.com>
2023-02-01 14:03:16 -0300

    https

--- rpg-front2/src/Cadastro.js
+++ rpg-front2/src/Cadastro.js
@@ -29,7 +29,7 @@ export default function Cadastro(props){
             return alert('Nome não pode ter mais que 15 caracteres')
         }
         try {//https://daniel-moura-rpg.herokuapp.com/
-            const resposta = await axios.post(`http://localhost:5000/api/cadastro`, {
+            const resposta = await axios.post(`https://localhost:5000/api/cadastro`, {
                 email: email, senha: senha,nome:nome
             })
             
