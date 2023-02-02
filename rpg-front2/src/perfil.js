25a30ed749f522fffddf501a08537983e64cca9a
daniel <daniel.bnh13@gmail.com>
2023-02-01 22:16:57 -0300

    a

--- rpg-front2/src/perfil.js
+++ rpg-front2/src/perfil.js
@@ -9,7 +9,7 @@ export default function Perfil(props){
     useEffect(() => {
            async function getpg3(){
             try{//https://daniel-moura-rpg.herokuapp.com/inimigos
-               const promessa=await axios.get('http://localhost:5000/api/perfil',{headers: {
+               const promessa=await axios.get('http://localhost/api/perfil',{headers: {
                    authorization : token
                 }})
                 setPerfil(promessa.data)
@@ -24,7 +24,7 @@ export default function Perfil(props){
          }, []);
           function mudarFoto(){
             const promise =  axios.put(
-                'http://localhost:5000/api/mudarfoto',
+                'http://localhost/api/mudarfoto',
                 {foto},{headers: {
                     authorization : token
                 }}
